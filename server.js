51014896ff40941cda9066acb46b9ff4649ae914
Justin Green <justingreen@Justins-MacBook-Air.local>
2022-02-11 17:49:32 -0800

    cors error again

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require("dotenv").config()
-const CLIENT_URL = process.env.NODE_ENV === "production" ? "https://kind-montalcini-8522f7.netlify.app/" : "http://localhost:3000"
+const CLIENT_URL = process.env.NODE_ENV === "production" ? "https://kind-montalcini-8522f7.netlify.app" : "http://localhost:3000"
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors')
